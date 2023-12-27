79cc58642dea9b0b8c38f884c2b150b1d5bd0416
henriits <121551949+henriits@users.noreply.github.com>
2023-12-27 22:10:47 +0200

    created simple gui for todos

--- customTK.py
+++ customTK.py
@@ -9,6 +9,7 @@ root.geometry("500x350")
 
 def add():
     print("Added todo")
+    entry1.delete(0, "end")
     
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
@@ -19,11 +20,10 @@ label.pack(pady=12, padx=10)
 entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="enter todo...")
 entry1.pack(pady=12, padx=10)
 
-entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="enter todo...")
-entry2.pack(pady=12, padx=10)
-
 button = customtkinter.CTkButton(master=frame, text="ADD", command=add)
 button.pack(pady=12, padx=10)
 
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Done")
+checkbox.pack(pady=12, padx=10)
 
 root.mainloop()
