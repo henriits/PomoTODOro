eba8abb0ee32ea187c4f18956014a73d7c55ff5d
henriits <121551949+henriits@users.noreply.github.com>
2023-12-28 23:27:50 +0200

    updated layout

--- main.py
+++ main.py
@@ -1,44 +1,42 @@
 import sys
-from PyQt6.QtWidgets import QWidget, QVBoxLayout , QLabel, QHBoxLayout, QApplication, QPushButton
-from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QWidget, QVBoxLayout , QLabel, QHBoxLayout, QApplication, QPushButton, QListWidget, QLineEdit
+from PyQt6.QtCore import Qt, QTimer, QTime
 
 class ToDoApp(QWidget):
     def __init__(self):
         super().__init__()
-        
- # Initialize the main layout
+
         self.layout = QVBoxLayout()
 
-        # Create a list to store tasks
+        self.task_list = QListWidget()
 
-
-        # Create input field and buttons
-
+        self.task_input = QLineEdit()
         self.add_button = QPushButton("Add Task")
         self.start_button = QPushButton("Start Pomodoro")
         self.timer_label = QLabel("Pomodoro Timer: 00:00")
         
-  
+        self.pomodoro_timer = QTimer(self)
+        self.pomodoro_duration = QTime(0, 1)  # 1 minute for each Pomodoro
+        self.break_duration = QTime(0, 0, 10)  # 10 seconds break
 
-        # Set up the layout
         self.setup_ui()
 
     def setup_ui(self):
         # Add widgets to the layout
         self.layout.addWidget(self.timer_label, alignment=Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignRight)
         self.layout.addWidget(QLabel("To-Do List"))
-
+        self.layout.addWidget(self.task_list)
+        self.layout.addWidget(self.task_input)
 
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.add_button)
         button_layout.addWidget(self.start_button)
         self.layout.addLayout(button_layout)
 
-        # Set the main layout for the window
         self.setLayout(self.layout)
 
 
-        # Set up the application window
+
         self.setWindowTitle("Pomodoro To-Do List App")
         self.setGeometry(100, 100, 400, 300)
         
