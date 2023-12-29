3ba8206d5f5ba6f2ae2463b5fec78f81893ddfb8
henriits <121551949+henriits@users.noreply.github.com>
2023-12-29 15:58:24 +0200

    split classes

--- main.py
+++ main.py
@@ -12,6 +12,31 @@ from PyQt6.QtWidgets import (
 from PyQt6.QtCore import Qt, QTimer, QTime
 
 
+class PomodoroTimer(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.timer_label = QLabel("Pomodoro Timer: 00:00")
+        self.pomodoro_timer = QTimer(self)
+        self.pomodoro_duration = QTime(0, 1)  # 1 minute for each Pomodoro
+        self.break_duration = QTime(0, 0, 10)  # 10 seconds break
+        self.is_pomodoro = False
+
+        self.setup_ui()
+
+    def setup_ui(self):
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.timer_label, alignment=Qt.AlignmentFlag.AlignBottom | Qt.AlignmentFlag.AlignRight)
+
+    def start_timer(self):
+        self.pomodoro_timer.timeout.connect(self.update_timer)
+        self.pomodoro_timer.start(1000)  # Timer triggers every second
+
+    def update_timer(self):
+        # Implement timer logic here
+        pass
+
+
 class ToDoApp(QWidget):
     def __init__(self):
         super().__init__()
@@ -23,20 +48,12 @@ class ToDoApp(QWidget):
         self.task_input = QLineEdit()
         self.add_button = QPushButton("Add Task")
         self.start_button = QPushButton("Start Pomodoro")
-        self.timer_label = QLabel("Pomodoro Timer: 00:00")
 
-        self.pomodoro_timer = QTimer(self)
-        self.pomodoro_duration = QTime(0, 1)  # 1 minute for each Pomodoro
-        self.break_duration = QTime(0, 0, 10)  # 10 seconds break
+        self.pomodoro_timer = PomodoroTimer(self)
 
         self.setup_ui()
 
     def setup_ui(self):
-        # Add widgets to the layout
-        self.layout.addWidget(
-            self.timer_label,
-            alignment=Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignRight,
-        )
         self.layout.addWidget(QLabel("To-Do List"))
         self.layout.addWidget(self.task_list)
         self.layout.addWidget(self.task_input)
@@ -44,7 +61,9 @@ class ToDoApp(QWidget):
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.add_button)
         button_layout.addWidget(self.start_button)
+
         self.layout.addLayout(button_layout)
+        self.layout.addWidget(self.pomodoro_timer)
 
         self.setLayout(self.layout)
 
