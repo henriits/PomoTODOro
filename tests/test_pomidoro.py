5107ff2f0766625e7832436a463780f5767a4496
henriits <henritsarents@gmail.com>
2024-01-10 17:54:18 +0200

    update tests

--- tests/test_pomidoro.py
+++ tests/test_pomidoro.py
@@ -2,13 +2,20 @@ from PyQt6.QtWidgets import QApplication
 from pomodoro_timer import PomodoroTimer
 from PyQt6.QtCore import QTime
 
+START_MINUTES = 0
+START_SECONDS = 20
+SHORT_MINUTES = 0
+SHORT_SECONDS = 30
+LONG_MINUTES = 0
+LONG_SECONDS = 40
+
 
 def test_start_pomodoro_timer():
     app = QApplication([])
     pomodoro_timer = PomodoroTimer()
     pomodoro_timer.start_pomodoro()
     assert pomodoro_timer.isActive()
-    assert pomodoro_timer.pomodoro_duration == QTime(0, 0, 5)
+    assert pomodoro_timer.pomodoro_duration == QTime(0, START_MINUTES, START_SECONDS)
 
 
 def test_start_short_break_timer():
@@ -17,7 +24,7 @@ def test_start_short_break_timer():
 
     pomodoro_timer.start_short_break()
     assert pomodoro_timer.break_timer.isActive()
-    assert pomodoro_timer.break_duration == QTime(0, 0, 10)
+    assert pomodoro_timer.break_duration == QTime(0, SHORT_MINUTES, SHORT_SECONDS)
 
 
 def test_start_long_break_timer():
@@ -26,4 +33,4 @@ def test_start_long_break_timer():
 
     pomodoro_timer.start_long_break()
     assert pomodoro_timer.long_break_timer.isActive()
-    assert pomodoro_timer.break_duration == QTime(0, 0, 15)
+    assert pomodoro_timer.break_duration == QTime(0, LONG_MINUTES, LONG_SECONDS)
